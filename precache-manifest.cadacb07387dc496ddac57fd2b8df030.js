c50cb9ecd04e302aa71c025eb7abbad3b8e930fb
Nikhil Anand <nikhil@Nikhils-MacBook-Air.local>
2019-05-23 22:01:21 +0530

    Updates

---
+++ precache-manifest.cadacb07387dc496ddac57fd2b8df030.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adbc1216ca4f84997a9a06d88c791373",
+    "url": "/shoppy/index.html"
+  },
+  {
+    "revision": "3c39cb0c360cd59f5747",
+    "url": "/shoppy/static/css/main.ca45b544.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/shoppy/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "3c39cb0c360cd59f5747",
+    "url": "/shoppy/static/js/main.08be38af.chunk.js"
+  },
+  {
+    "revision": "8ddef8943929bef8d477",
+    "url": "/shoppy/static/js/runtime~main.eb273d13.js"
+  }
+]);
